feaa116f2e5d69ad597b4fe2b73121635d7b83c7
harshrajput018 <46439089+harshrajput018@users.noreply.github.com>
2023-11-14 00:47:09 +0530

    made projects container scrollable(6)

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,9 @@ const About = () => {
             
             <div className="About-content">
             <h2 >WHO I AM</h2>
-                I'm a passionate full-stack web developer with expertise in HTML, CSS, JavaScript, React, and Node.js. With [X years] of experience, I specialize in creating innovative and user-friendly web solutions. I'm committed to continuous learning and staying current with industry trends. Beyond coding, I enjoy [hobbies or interests]. Let's collaborate on your next project and turn your ideas into reality.
+               <div className='.para'>
+               I'm a passionate full-stack web developer with expertise in HTML, CSS, JavaScript, React, and Node.js. With [X years] of experience, I specialize in creating innovative and user-friendly web solutions. I'm committed to continuous learning and staying current with industry trends. Beyond coding, I enjoy [hobbies or interests]. Let's collaborate on your next project and turn your ideas into reality.
+                </div> 
             </div>
 
         </div>
